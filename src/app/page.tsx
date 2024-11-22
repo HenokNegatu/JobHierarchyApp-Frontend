14252bbfe640cd26962b2d301fdf34f4ba5c2324
Henok Negatu <henoknegatu111@gmail.com>
2024-11-22 14:26:01 +0300

    refactor: change mantine primary color | change port to 3001

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { Button } from '@mantine/core'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold mb-8">Job Hierarchy App</h1>
+      <div className="flex space-x-4">
+        <Link href="/positions">
+          <Button>View Job Positions</Button>
+        </Link>
+        <Link href="/tasks">
+          <Button>View Tasks</Button>
+        </Link>
+      </div>
     </div>
-  );
+  )
 }
