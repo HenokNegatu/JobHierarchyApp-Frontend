ca33d7014e82a73040bb6b9d7fefd2983136a8ad
Henok Negatu <henoknegatu111@gmail.com>
2024-11-25 15:51:43 +0300

    style: change task page bg to gray

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -4,7 +4,7 @@ import { Button } from '@mantine/core'
 
 export default function TasksPage() {
   return (
-    <div className="space-y-8">
+    <div className="space-y-8 bg-gray-100">
       <Link href='/positions' className="text-lg font-bold m-4"><Button>Show Job Positions</Button></Link>
       <TaskManagement />
     </div>
