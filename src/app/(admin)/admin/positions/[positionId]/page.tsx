b272e4dbcfb9355921f63c8bd29aa9a99fc20020
Henok Negatu <henoknegatu111@gmail.com>
2024-12-02 17:13:55 +0300

    refactor: add header to positions page

--- src/app/(admin)/admin/positions/[positionId]/page.tsx
+++ src/app/(admin)/admin/positions/[positionId]/page.tsx
@@ -14,9 +14,14 @@ export default function Position({ params }: PositionsProp) {
 
     return (
         <div className="flex flex-col h-full">
-            <Button variant="light" onClick={open} className="self-end my-2 bg-transparent">
-                <PanelRight />
-            </Button>
+            <header className="bg-white shadow-sm mb-2">
+                <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-2 flex justify-end">
+                    <Button variant="light" onClick={open} >
+                        <PanelRight size={30} />
+                    </Button>
+                </div>
+
+            </header>
             <PositionInfo positionId={params.positionId} />
         </div>
     )
